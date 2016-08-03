1f26565296bf080d299dd2f3fef62bc90c61ea56
wuchao <jhan_wuchao02@163.com>
2016-08-03 18:12:13 +0800

    add AutoTestBase class,add Directory

---
+++ src/main/java/com/pingan/autotest_ui/object_page/a.java
@@ -0,0 +1,7 @@
+package com.pingan.autotest_ui.object_page;
+
+/**
+ * Created by wuchao on 16/8/3.
+ */
+public class a {
+}
---
+++ src/main/java/com/pingan/autotest_ui/util/a.java
@@ -0,0 +1,7 @@
+package com.pingan.autotest_ui.util;
+
+/**
+ * Created by wuchao on 16/8/3.
+ */
+public class a {
+}
